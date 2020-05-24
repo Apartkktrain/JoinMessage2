57f897355a3d1a59f196ef0365f3b333dd97d36d
Apartkktrain <64818647+Apartkktrain@users.noreply.github.com>
2020-05-24 09:28:13 +0900

    Add files via upload

---
+++ src/apartkktrain/tokyo/Main.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace apartkktrain\tokyo;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\Player;
+
+
+class Main extends pluginBase implements Listener
+{
+    public function onEnable()
+    {
+        $this->getLogger()->notice("tokyoserverSystemsver.0.0.1の読み込み成功");
+        $this->getServer()->getPluginManager()->registerEvents($this,$this);
+    }
+
+
+
+public function onJoin(PlayerJoinEvent $event)
+ {
+     $event->getPlayer()->fry = false;
+    $name = $event->getPlayer()->getName();
+    $event->setJoinMessage("§a{$name}が参加しました。鯖生活を楽しんでいってね!");
+ }
+
+}
